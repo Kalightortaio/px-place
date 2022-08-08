55accffffb0f1bc48ec95a1fb79cc29782e7da7e
Kalightortaio <kokataykrishna@gmail.com>
2022-08-08 14:42:22 -0700

    client side only clickable cells

--- components/Cell.tsx
+++ components/Cell.tsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import styles from '../styles/Cell.module.css'
 
 export default function Cell() {
-    return <div className={styles.container}>
-    </div>
+    const [isActive, setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(current => !current);
+    };
+
+    return (
+        <div 
+            className={styles.container} 
+            onClick={handleClick} 
+            style={{
+                backgroundColor: isActive ? 'black' : '',
+            }}>
+        </div>
+    );
 }
